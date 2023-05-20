2a46ea0f1f440125636a9b0d228f8f194eeccef9
blacknii <42908977+blacknii@users.noreply.github.com>
2023-05-20 11:22:41 +0200

    .

--- app.php
+++ app.php
@@ -4,27 +4,62 @@ require_once "game.php";
 
 $game = new Game();
 $frame = 1;
-$isGameOver = false;
+$isRollValid = false;
 
 echo "Gra w kręgle: wprowadź liczbę strąconych kręgli w każdym rzucie i naciśnij Enter.\n";
 
 while ($frame <= 10) {
-  echo "Frame {$frame}, rzut 1: ";
-  $roll1 = (int) fgets(STDIN);
+  while (!$isRollValid) {
+    echo "Frame {$frame}, rzut 1: ";
+    $roll1 = (int) fgets(STDIN);
+    if ($roll1 >= 0 && $roll1 <= 10) {
+      $isRollValid = true;
+    } else {
+      echo "Błąd - Rzut może mieć wartość od 1 do 10 podaj wartość jeszcze raz." .
+        "\n";
+    }
+  }
+  $isRollValid = false;
 
   $game->roll($roll1);
+
   echo "Aktualna liczba punktów: " . $game->getScore() . "\n";
 
   if ($roll1 != 10 || $frame == 10) {
-    echo "Frame {$frame}, rzut 2: ";
-    $roll2 = (int) fgets(STDIN);
+    while (!$isRollValid) {
+      echo "Frame {$frame}, rzut 2: ";
+      $roll2 = (int) fgets(STDIN);
+      if ($roll2 >= 0 && $roll2 <= 10 - ($roll1 % 10)) {
+        $isRollValid = true;
+      } else {
+        echo "Błąd - Rzut może mieć wartość od 1 do " .
+          10 -
+          ($roll1 % 10) .
+          " podaj wartość jeszcze raz." .
+          "\n";
+      }
+    }
+    $isRollValid = false;
 
     $game->roll($roll2);
     echo "Aktualna liczba punktów: " . $game->getScore() . "\n";
 
     if ($frame == 10 && $roll1 + $roll2 >= 10) {
-      echo "Frame {$frame}, dodatkowy rzut: ";
-      $extraRoll = (int) fgets(STDIN);
+      while (!$isRollValid) {
+        echo "Frame {$frame}, dodatkowy rzut: ";
+        $extraRoll = (int) fgets(STDIN);
+        if ($extraRoll >= 0 && $extraRoll <= $roll1 - ($roll2 % 10)) {
+          $isRollValid = true;
+        } else {
+          echo "Błąd - Rzut może mieć wartość od 1 do " .
+            $roll1 -
+            ($roll2 % 10) .
+            " podaj wartość jeszcze raz." .
+            "\n";
+        }
+      }
+      $isRollValid = false;
+
       $game->roll($extraRoll);
     }
   }
